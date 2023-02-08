e9c42702a2cf8c107f330fc27af6583556559776
stc <standardtemplateconstruct2022@gmail.com>
2023-02-08 10:24:47 +0200

    Initial Commit

---
+++ BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by standard on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
