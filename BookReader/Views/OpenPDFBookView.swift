b5a3d96a3824e017d265e6dd2af75cd09d9da35e
stc <standardtemplateconstruct2022@gmail.com>
2023-02-11 12:14:47 +0200

    refactoring

--- BookReader/Views/OpenPDFBookView.swift
+++ BookReader/Views/OpenPDFBookView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 import PDFKit
 import AVFoundation
+extension CGPoint: HashableSynthesizable { }
+extension CGRect: HashableSynthesizable { }
+
 
 struct OpenPDFBookView: View {
     @Environment(\.presentationMode) var presentationMode
@@ -98,27 +101,6 @@ struct OpenPDFBookView: View {
 
 }
 
-public protocol HashableSynthesizable: Hashable { }
-
-public extension HashableSynthesizable {
-  static func == (hashable0: Self, hashable1: Self) -> Bool {
-    zip(hashable0.hashables, hashable1.hashables).allSatisfy(==)
-  }
-
-  func hash(into hasher: inout Hasher) {
-    hashables.forEach { hasher.combine($0) }
-  }
-}
-
-private extension HashableSynthesizable {
-  var hashables: [AnyHashable] {
-    Mirror(reflecting: self).children
-      .compactMap { $0.value as? AnyHashable }
-  }
-}
-
-extension CGPoint: HashableSynthesizable { }
-extension CGRect: HashableSynthesizable { }
 
 
 @objc class PageOverlay : UIView {
