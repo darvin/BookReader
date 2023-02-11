6bae77d47d44fd6293a9642d935386708cae5ea1
stc <standardtemplateconstruct2022@gmail.com>
2023-02-11 12:57:21 +0200

    pdf view refactoring

--- BookReader/Views/PDFViewer/OpenPDFBookView.swift
+++ BookReader/Views/PDFViewer/OpenPDFBookView.swift
@@ -22,18 +22,39 @@ struct OpenPDFBookView: View {
     init(book: any BookPDFable) {
         _viewModel = StateObject(wrappedValue: OpenBookViewModel(book: book))
     }
+    
+    private let pdfViewHandler = PDFViewHandler()
+    private let pdfView = {
+        
+        let p = PDFView()
+
+        p.autoScales = true
+        p.pageShadowsEnabled = false
+
+        #if true
+            p.backgroundColor = UIColor.red
+            p.pageBreakMargins = UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+        #else
+            p.pageBreakMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        #endif
+        return p
+    }()
 
     var body: some View {
         //FIXME causes cycle error, fixme
         if let data = viewModel.pdfData {
-            PDFKitRepresentedView(data)
+
+            PDFKitRepresentedView(pdfView: pdfView)
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                 .background(Color.red)
                 .edgesIgnoringSafeArea(.all)
                 .navigationBarHidden(true)
                 .onAppear {
-                            UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
-                            AppDelegate.orientationLock = .landscape
+                    UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+                    AppDelegate.orientationLock = .landscape
+                    pdfViewHandler.pdfView = pdfView
+                    pdfView.document = PDFDocument(data: data)
+
                 }
                 .onDisappear {
                     
--- BookReader/Views/PDFViewer/PDFRepresentedView.swift
+++ BookReader/Views/PDFViewer/PDFRepresentedView.swift
@@ -14,39 +14,16 @@ import PDFKit
 struct PDFKitRepresentedView: UIViewRepresentable {
     typealias UIViewType = PDFView
 
-    let data: Data
-    let singlePage: Bool
+    let pdfView: PDFView
     let pdfViewHandler = PDFViewHandler()
 
-    init(_ data: Data, singlePage: Bool = false) {
-        self.data = data
-        self.singlePage = singlePage
-    }
-    
     func makeUIView(context _: UIViewRepresentableContext<PDFKitRepresentedView>) -> UIViewType {
 
-        
-        let pdfView = PDFView()
-        pdfViewHandler.pdfView = pdfView
-
-        pdfView.document = PDFDocument(data: data)
-        pdfView.autoScales = true
-        pdfView.pageShadowsEnabled = false
-
-        #if true
-            pdfView.backgroundColor = UIColor.red
-            pdfView.pageBreakMargins = UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
-        #else
-            pdfView.pageBreakMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        #endif
-        if singlePage {
-            pdfView.displayMode = .singlePage
-        }
         return pdfView
     }
 
     func updateUIView(_ pdfView: UIViewType, context _: UIViewRepresentableContext<PDFKitRepresentedView>) {
-        pdfView.document = PDFDocument(data: data)
+ 
     }
     
     
