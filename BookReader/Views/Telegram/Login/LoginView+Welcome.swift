6b16c0fc4cc7a57c3ca254c64f674d29518ef99d
stc <standardtemplateconstruct2022@gmail.com>
2023-02-15 11:31:01 +0200

    stuff

--- BookReader/Views/Telegram/Login/LoginView+Welcome.swift
+++ BookReader/Views/Telegram/Login/LoginView+Welcome.swift
@@ -11,52 +11,27 @@ import TDLibKit
 extension LoginView {
     var welcome: some View {
         VStack {
-            if showLogo {
-                Image("WelcomeScreenImage")
-                    .resizable()
-                    .frame(width: showContent ? 250 : 320, height: showContent ? 250 : 320)
-                    .padding(.top)
-                    .transition(.scale)
-                    .zIndex(1)
-            }
             Group {
-                if showContent {
-                    Text("Welcome to Moc!")
-                        .font(.system(size: 36, weight: .bold))
-                    Text("Choose your login method")
-                    Button {
-                        Task {
-                            try? await service.requestQrCodeAuth()
-                        }
-                    } label: {
-                        Label("Continue using QR Code", systemImage: "qrcode")
+                Text("Login To Telegram")
+                    .font(.system(size: 36, weight: .bold))
+                Text("Choose your login method")
+                Button {
+                    Task {
+                        try? await service.requestQrCodeAuth()
                     }
-                    .buttonStyle(.borderedProminent)
-                    .controlSize(.large)
-                    .padding(.bottom, 8)
-                    .padding(.top, 56)
-                    Button {
-                        openedScreen = .phoneNumber
-                    } label: {
-                        Label("Continue using phone number", systemImage: "phone")
-                    }
-                    .controlSize(.large)
+                } label: {
+                    Label("Continue using QR Code", systemImage: "qrcode")
                 }
-            }
-            .transition(.move(edge: .top).combined(with: .opacity))
-        }
-        .animation(.spring(dampingFraction: 0.6), value: showLogo)
-        .animation(.spring(), value: showContent)
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                withAnimation {
-                    showLogo = true
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                .padding(.bottom, 8)
+                .padding(.top, 56)
+                Button {
+                    openedScreen = .phoneNumber
+                } label: {
+                    Label("Continue using phone number", systemImage: "phone")
                 }
-            }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                withAnimation {
-                    showContent = true
-                }
+                .controlSize(.large)
             }
         }
     }
