5e092a063e4ac7696fdf69986bd41facc9bc5050
stc <standardtemplateconstruct2022@gmail.com>
2023-03-26 12:46:34 +0300

    good scrolling

--- Plugins/PDFViewer/Sources/PDFViewer/MyPDFView.swift
+++ Plugins/PDFViewer/Sources/PDFViewer/MyPDFView.swift
@@ -15,7 +15,7 @@ public class MyPDFView: PDFView {
 
     private var pdfViewHandler: PDFViewHandler
 
-    public override func willMove(toWindow newWindow: UIWindow?) {
+    override public func willMove(toWindow _: UIWindow?) {
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(nextPage(notification:)),
@@ -34,10 +34,11 @@ public class MyPDFView: PDFView {
         NotificationCenter.default.removeObserver(self)
     }
 
-    @objc func nextPage(notification: Notification) {
+    @objc func nextPage(notification _: Notification) {
         turnPage()
     }
-    @objc func prevPage(notification: Notification) {
+
+    @objc func prevPage(notification _: Notification) {
         turnPage(false)
     }
 
@@ -59,42 +60,42 @@ public class MyPDFView: PDFView {
         pdfViewHandler.pdfView = self
     }
 
-    required init?(coder: NSCoder) {
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
-    public func turnPage(_ isForwardDirection: Bool = true) {
-        if let currentPage = currentPage, let document = document {
-            let visibleRect = CGRect(origin: CGPointZero, size: bounds.size)
-            let currentRect = convert(visibleRect, to: currentPage)
-
-            let y =
-                isForwardDirection
-                ? currentRect.origin.y - currentRect.size.height
-                : currentRect.origin.y + currentRect.size.height
-
-            var newPage = currentPage
-            print("y \(y) maxY \(currentPage.bounds(for: .mediaBox).size.height)")
-            if (y + 2.0) > currentPage.bounds(for: .mediaBox).size.height {
-                if let prevPage = document.page(at: document.index(for: currentPage) - 1) {
-                    newPage = prevPage
-                    //fixme coords
-                }
+extension PDFView {
+    var scrollView: UIScrollView? {
+        for subview in subviews {
+            if let scrollView = subview as? UIScrollView {
+                return scrollView
+            } else if let pdfView = subview as? PDFView {
+                return pdfView.scrollView
+            } else if let scrollView = subview.subviews.lazy.compactMap({ $0 as? UIScrollView }).first {
+                return scrollView
+            } else if let pdfView = subview.subviews.lazy.compactMap({ $0 as? PDFView }).first {
+                return pdfView.scrollView
             }
-
-            let nextRect = CGRect(
-                origin:
-                    CGPoint(
-                        x: currentRect.origin.x,
-                        y: y
-                    ),
-                size: currentRect.size
-            )
-
-            print("CURR: \(currentRect)")
-            print("NEXT: \(nextRect)")
-            go(to: nextRect, on: newPage)
         }
+        return nil
+    }
+}
 
+public extension PDFView {
+    func turnPage(_ isForwardDirection: Bool = true) {
+        guard let scrollView = scrollView else { return }
+
+        let offsetY = scrollView.contentOffset.y + (isForwardDirection ? scrollView.bounds.height : -scrollView.bounds.height)
+        let offsetX = scrollView.contentOffset.x
+        let contentSize = scrollView.contentSize
+
+        if offsetY >= 0, offsetY <= contentSize.height - scrollView.bounds.height {
+            let newOffset = CGPoint(x: offsetX, y: offsetY)
+            UIView.animate(withDuration: 0.03) {
+                scrollView.contentOffset = newOffset
+            }
+        }
     }
 }
